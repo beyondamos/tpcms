32b782036f7c00c2be7d4e608524a839a2d7c93f
beyondamos <beyondamos@gmail.com>
2017-04-13 17:18:09 +0800

    完成了后台登录控制

--- Application/Admin/Controller/CommonController.class.php
+++ Application/Admin/Controller/CommonController.class.php
@@ -5,5 +5,29 @@ use Think\Controller;
  * 后台公共控制器
  */
 class CommonController extends Controller{
-	
+
+	/**
+	 * 初始化控制器
+	 * @return [type] [description]
+	 */
+	public function _initialize(){
+		$array_filter = array('Login/index','Login/verify');
+		$current = CONTROLLER_NAME.'/'.ACTION_NAME;
+		if(!in_array($current,$array_filter)){
+			if(!$this->isLogin()) $this->error('您尚未登录！',U('Login/index'),2);
+		}
+		
+	}
+
+
+
+	/**
+	 * 判断是否已经登录
+	 * @return boolean [description]
+	 */
+	private function isLogin(){
+		if(session('user_id') && session('user_id') > 0) return true;
+		return false;
+	}
+
 }
