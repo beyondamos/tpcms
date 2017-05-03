9151a58e4c202491f73e79078d190c83ae212172
beyondamos <beyondamos@gmail.com>
2017-05-03 16:11:16 +0800

    完成广告功能

--- Application/Admin/Controller/AdvController.class.php
+++ Application/Admin/Controller/AdvController.class.php
@@ -13,8 +13,67 @@ class AdvController extends CommonController{
     public function index(){
         $adv_model = D('Adv');
         $adv_data = $adv_model->select();
-        $this->assign('adv_model', $adv_data);
+        $this->assign('adv_data', $adv_data);
         $this->display();
     }
+
+    /**
+     * 广告添加
+     */
+    public function add(){
+        if(IS_POST){
+            $adv_model = D('Adv');
+            if($adv_model->create()){
+                if($adv_model->add()){
+                    $this->success('广告添加成功', U('Adv/index'), 1);
+                }else{
+                    $this->error('广告添加失败');
+                }
+            }else{
+                $this->error($adv_model->getError());
+            }
+        }else{
+            $this->display();
+        }
+    }
+
+    /**
+     * 广告编辑
+     */
+    public function edit(){
+        if(IS_POST){
+
+            $adv_model = D('Adv');
+            if($adv_model->create()){
+                if($adv_model->save()){
+                    $this->success('广告编辑成功', U('Adv/index'), 1);
+                }else{
+                    $this->error('广告编辑失败');
+                }
+            }else{
+                $this->error($adv_model->getError());
+            }
+        }else{
+            $id = I('get.id');
+            $adv_model = D('Adv');
+            $adv_data = $adv_model->find($id);
+            $this->assign('adv_data',$adv_data);
+            $this->display();
+        }
+    }
+
+    /**
+     * 广告删除
+     */
+    public function delete(){
+        $id = I('get.id');
+        $adv_model = D('Adv');
+        if($adv_model->delete($id)){
+            $this->success('广告删除成功', U('Adv/index'), 1);
+        }else{
+            $this->error('广告删除失败');
+        }
+    }
+
 
 }
--- Application/Common/Model/AdvModel.class.php
+++ Application/Common/Model/AdvModel.class.php
@@ -6,5 +6,9 @@ namespace Common\Model;
 use Think\Model;
 
 class AdvModel extends Model{
-
+    protected $_validate = array(
+        array('adv_name', 'require', '广告名称必须填写', self::EXISTS_VALIDATE, 'regex', self::MODEL_BOTH),
+        array('adv_position', 'require', '广告位置必须填写', self::EXISTS_VALIDATE, 'regex', self::MODEL_BOTH),
+        array('adv_code', 'require', '广告代码必须填写', self::EXISTS_VALIDATE, 'regex', self::MODEL_BOTH),
+    );
 }
